98568215a59a8c91f988522f3627516d03fab3a9
spencer <spencer@homelightarchive.com>
2023-02-02 11:19:50 -0700

    started styling form

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -6,19 +6,28 @@ const NewRecipeScreen = () => {
     <section>
       <h1>Tell us about your Recipe!</h1>
       <form action="">
-        <input type="text" id="name" placeholder="Name"/>
-        <input type="text" id="image" placeholder="Image URL"/>
-        <input type="radio" name="type" id="cook" />
-        <label htmlFor="cook">Cook</label>
-        <input type="radio" name="type" id="bake" />
-        <label htmlFor="bake">Bake</label>
-        <input type="radio" name="type" id="drink" />
-        <label htmlFor="drink">Drink</label>
-        <input type="text" id="prepTime" placeholder="Prep Time" />
-        <input type="text" id="cookTime" placeholder="Cook Time" />
-        <input type="text" id="serve" placeholder="Serves" />
-        <input type="text" id="ingredient" placeholder="Ingredient" />
-        <input type="number" id="quantity" placeholder="Quanity"/>
+        <div className="row">
+          <input type="text" id="name" placeholder="Name"/>
+          <input type="text" id="image" placeholder="Image URL"/>
+        </div>
+        <div className="row">
+          <input type="radio" name="type" id="cook" />
+          <label htmlFor="cook">Cook</label>
+          <input type="radio" name="type" id="bake" />
+          <label htmlFor="bake">Bake</label>
+          <input type="radio" name="type" id="drink" />
+          <label htmlFor="drink">Drink</label>
+        </div>
+        <div className="row">
+          <input type="text" id="prepTime" placeholder="Prep Time" />
+          <input type="text" id="cookTime" placeholder="Cook Time" />
+          <input type="text" id="serve" placeholder="Serves" />
+        </div>
+        <div className="row">
+          
+          <input type="text" id="ingredient" placeholder="Ingredient" />
+          <input type="number" id="quantity" placeholder="Quanity"/>
+        </div>
         <button type="number" id="addIngredient">Add Another</button>
         <textarea id="instructions" placeholder="What are the instructions?"></textarea>
         <input type="submit" value="Save"/>
