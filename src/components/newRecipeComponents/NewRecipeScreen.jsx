9d10f667d3cbd7d37d1ebb4eb7f35e23e8563276
spencer <spencer@homelightarchive.com>
2023-02-06 15:43:22 -0700

    tweaked recipe screen

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -7,10 +7,10 @@ const NewRecipeScreen = () => {
       <h1>Tell us about your Recipe!</h1>
       <form action="">
         <div className="row">
-          <input type="text" id="name" placeholder="Name"/>
-          <input type="text" id="image" placeholder="Image URL"/>
+          <input type="text" id="name" className="half" placeholder="Name"/>
+          <input type="text" id="image" className="half" placeholder="Image URL"/>
         </div>
-        <div className="row">
+        <div className="row" style={{justifyContent: 'space-evenly'}}>
           <div className="option">
           <input type="radio" name="type" id="cook" />
           <label htmlFor="cook">Cook</label>
@@ -25,16 +25,18 @@ const NewRecipeScreen = () => {
           </div>
         </div>
         <div className="row">
-          <input type="text" id="prepTime" placeholder="Prep Time" />
-          <input type="text" id="cookTime" placeholder="Cook Time" />
-          <input type="text" id="serve" placeholder="Serves" />
+          <input type="text" id="prepTime" className="third" placeholder="Prep Time" />
+          <input type="text" id="cookTime" className="third" placeholder="Cook Time" />
+          <input type="text" id="serve" className="third" placeholder="Serves" />
         </div>
         <div className="row">
+          <div className="half">
           <input type="text" id="ingredient" placeholder="Ingredient" />
           <input type="number" id="quantity" placeholder="Quanity"/>
-          <ul></ul>
+          </div>
+          <ul className="half"></ul>
         </div>
-        <button type="number" id="addIngredient">Add Another</button>
+        <button id="addIngredient">Add Another</button>
         <textarea id="instructions" placeholder="What are the instructions?"></textarea>
         <input type="submit" value="Save"/>
       </form>
