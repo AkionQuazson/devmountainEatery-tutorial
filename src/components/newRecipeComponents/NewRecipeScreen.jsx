05ca4c15119368cd0bc865e05932c5cc6f2871c8
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2023-02-07 20:50:40 -0700

    form only needs styling

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,46 +1,86 @@
 import React from "react";
+import {Formik, Form} from 'formik';
 import '../../css/newRecipe.css';
+import { useState } from "react";
+import axios from "axios";
 
 const NewRecipeScreen = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [ingName, setIngName] = useState('');
+  const [ingQuant, setIngQuant] = useState('');
+
+  const initialValues = {
+    type: "",
+    recipeName: "",
+    imageURL: "",
+    prepTime: "",
+    cookTime: "",
+    serves: "",
+    ingredients: [],
+    instructions: "",
+  };
+
+  const onSubmit = (values) => {
+    if (values.type.length > 0 && values.recipeName.length > 0 && values.imageURL.length > 0 && values.prepTime.length > 0 && values.cookTime.length > 0 && values.serves.length > 0 && values.instructions.length > 0) {
+      axios
+      .post(`https://recipes.devmountain.com/recipes`, values)
+    }
+    else {
+      alert("Input data");
+    }
+  }
+
+  const addIngredient = (e) => {
+    e.preventDefault();
+    setIngredients([...ingredients, {name:ingName, quantity:ingQuant}])
+  }
+
+  const renderIngredients = ingredients.map((ingredient, i) => {
+    return <li key={i}>{ingredient.quantity} {ingredient.name}</li>
+  })
+
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
-      <form action="">
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({values, handleChange, handleSubmit}) => {
+        return <Form onSubmit={handleSubmit}>
         <div className="row">
-          <input type="text" id="name" className="half" placeholder="Name"/>
-          <input type="text" id="image" className="half" placeholder="Image URL"/>
+          <input type="text" id="recipeName" className="half" placeholder="Name" onChange={handleChange} value={values.recipeName}/>
+          <input type="text" id="imageURL" className="half" placeholder="Image URL" onChange={handleChange} value={values.imageURL}/>
         </div>
         <div className="row" style={{justifyContent: 'space-evenly'}}>
           <div className="option">
-          <input type="radio" name="type" id="cook" />
+          <input type="radio" name="type" id="cook" onChange={handleChange} value="Cook"/>
           <label htmlFor="cook">Cook</label>
           </div>
           <div className="option">
-          <input type="radio" name="type" id="bake" />
+          <input type="radio" name="type" id="bake" onChange={handleChange} value="Bake"/>
           <label htmlFor="bake">Bake</label>
           </div>
           <div className="option">
-          <input type="radio" name="type" id="drink" />
+          <input type="radio" name="type" id="drink" onChange={handleChange} value="Drink"/>
           <label htmlFor="drink">Drink</label>
           </div>
         </div>
         <div className="row">
-          <input type="text" id="prepTime" className="third" placeholder="Prep Time" />
-          <input type="text" id="cookTime" className="third" placeholder="Cook Time" />
-          <input type="text" id="serve" className="third" placeholder="Serves" />
+          <input type="text" id="prepTime" className="third" placeholder="Prep Time" onChange={handleChange} value={values.prepTime}/>
+          <input type="text" id="cookTime" className="third" placeholder="Cook Time" onChange={handleChange} value={values.cookTime}/>
+          <input type="text" id="serves" className="third" placeholder="Serves" onChange={handleChange} value={values.serves}/>
         </div>
         <div className="row">
           <div className="half">
-          <input type="text" id="ingredient" placeholder="Ingredient" />
-          <input type="number" id="quantity" placeholder="Quanity"/>
+          <input type="text" id="ingredient" placeholder="Ingredient" onChange={(e) => setIngName(e.target.value)} value={ingName}/>
+          <input type="text" id="quantity" placeholder="Quanity" onChange={(e) => setIngQuant(e.target.value)} value={ingQuant}/>
           </div>
-          <ul className="half"></ul>
+          <ul className="half">{renderIngredients}</ul>
         </div>
-        <button id="addIngredient">Add Another</button>
-        <textarea id="instructions" placeholder="What are the instructions?"></textarea>
+        <button id="addIngredient" onClick={addIngredient}>Add Another</button>
+        <textarea id="instructions" placeholder="What are the instructions?" onChange={handleChange} value={values.instructions}></textarea>
         <input type="submit" value="Save"/>
-      </form>
-    </section>
+      </Form>
+    }}</Formik>
+  </section>
   );
 };
 
